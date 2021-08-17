9d6fdc7c2bda9f4381aeb783d7ca3f86959f35ce
geni94 <eugen.yzeiri@gmail.com>
2021-08-17 22:19:52 +0200

    connection fixes

--- src/services/api.js
+++ src/services/api.js
@@ -1,15 +1,16 @@
 export const testApi = 'http://localhost:3000';
+const BaseUrl = process.env?.VUE_APP_API_ENDPOINT || 'http://localhost:3000';
 
-export const allUsers = `${process.env.VUE_APP_API_ENDPOINT}/api/v1/users`;
-export const user = `${process.env.VUE_APP_API_ENDPOINT}/api/v1/user`;
+export const allUsers = `${BaseUrl}/api/v1/users`;
+export const user = `${BaseUrl}/api/v1/user`;
 
-export const RegisterUser = `${process.env.VUE_APP_API_ENDPOINT}/api/user/register`;
-export const LoginUser = `${process.env.VUE_APP_API_ENDPOINT}/api/user/login`;
-export const GetUser = `${process.env.VUE_APP_API_ENDPOINT}/api/user/me`;
+export const RegisterUser = `${BaseUrl}/api/user/register`;
+export const LoginUser = `${BaseUrl}/api/user/login`;
+export const GetUser = `${BaseUrl}/api/user/me`;
 
-export const GetUserConfig = `${process.env.VUE_APP_API_ENDPOINT}/api/user/get-sp-auth`;
-export const SetUserConfig = `${process.env.VUE_APP_API_ENDPOINT}/api/user/save-sp-auth`;
+export const GetUserConfig = `${BaseUrl}/api/user/get-sp-auth`;
+export const SetUserConfig = `${BaseUrl}/api/user/save-sp-auth`;
 
-export const GetInventories = `${process.env.VUE_APP_API_ENDPOINT}/api/inventories/get-inventories`;
-export const GetOrders = `${process.env.VUE_APP_API_ENDPOINT}/api/get-orders`;
-export const GetLatestOrders = `${process.env.VUE_APP_API_ENDPOINT}/api/get-latest-orders`;
+export const GetInventories = `${BaseUrl}/api/inventories/get-inventories`;
+export const GetOrders = `${BaseUrl}/api/get-orders`;
+export const GetLatestOrders = `${BaseUrl}/api/get-latest-orders`;
